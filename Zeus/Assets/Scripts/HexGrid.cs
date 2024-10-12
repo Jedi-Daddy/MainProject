37928e4506359579c56b9b02eda078e2897fdffb
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-10-12 16:07:01 +0200

    start to create Hex map

---
+++ Zeus/Assets/Scripts/HexGrid.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+
+
+
+}
