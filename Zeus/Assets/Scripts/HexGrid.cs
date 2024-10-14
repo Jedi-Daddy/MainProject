c2b8e7ef77e677b81f6976bb8df82e3b36989e33
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-10-14 17:40:17 +0200

    create simple map editor

--- Zeus/Assets/Scripts/HexGrid.cs
+++ Zeus/Assets/Scripts/HexGrid.cs
@@ -5,20 +5,19 @@ using UnityEngine.UI;
 
 public class HexGrid : MonoBehaviour
 {
+
     public int width = 6;
     public int height = 6;
 
-    public HexCell cellPrefab;
+    public Color defaultColor = Color.white;
 
+    public HexCell cellPrefab;
     public Text cellLabelPrefab;
 
-    public Color defaultColor = Color.white;
-    public Color touchedColor = Color.magenta;
+    HexCell[] cells;
 
     Canvas gridCanvas;
     HexMesh hexMesh;
-
-    HexCell[] cells;
 
     void Awake()
     {
@@ -41,6 +40,16 @@ public class HexGrid : MonoBehaviour
         hexMesh.Triangulate(cells);
     }
 
+    public void ColorCell(Vector3 position, Color color)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        cell.color = color;
+        hexMesh.Triangulate(cells);
+    }
+
     void CreateCell(int x, int z, int i)
     {
         Vector3 position;
@@ -59,33 +68,5 @@ public class HexGrid : MonoBehaviour
         label.rectTransform.anchoredPosition =
             new Vector2(position.x, position.z);
         label.text = cell.coordinates.ToStringOnSeparateLines();
-    }
-
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            HandleInput();
-        }
-    }
-
-    void HandleInput()
-    {
-        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(inputRay, out hit))
-        {
-            TouchCell(hit.point);
-        }
-    }
-
-    public void TouchCell(Vector3 position)
-    {
-        position = transform.InverseTransformPoint(position);
-        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
-        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
-        HexCell cell = cells[index];
-        cell.color = touchedColor;
-        hexMesh.Triangulate(cells);
     }
 }
