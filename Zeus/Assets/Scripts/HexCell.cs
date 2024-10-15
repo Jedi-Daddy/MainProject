e26ab561533a67472f21d614ce4984c24f45f3c4
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-10-15 12:24:21 +0200

    add cells connected

--- Zeus/Assets/Scripts/HexCell.cs
+++ Zeus/Assets/Scripts/HexCell.cs
@@ -10,4 +10,15 @@ public class HexCell : MonoBehaviour
 
     [SerializeField]
     HexCell[] neighbors;
+
+    public HexCell GetNeighbor(HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
 }
