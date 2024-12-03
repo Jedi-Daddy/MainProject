9778e617b7eecb0bb5d805c27e191e3eeb0459e4
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-12-03 08:59:44 +0100

    refactoring for rivers and roads memory

--- Zeus/Assets/Scripts/HexMapEditor.cs
+++ Zeus/Assets/Scripts/HexMapEditor.cs
@@ -269,6 +269,7 @@ public class HexMapEditor : MonoBehaviour
                 new BinaryWriter(File.Open(path, FileMode.Create))
         )
         {
+            writer.Write(0);
             hexGrid.Save(writer);
         }
     }
@@ -278,7 +279,15 @@ public class HexMapEditor : MonoBehaviour
         string path = Path.Combine(Application.persistentDataPath, "test.map");
         using (BinaryReader reader = new BinaryReader(File.OpenRead(path)))
         {
-            hexGrid.Load(reader);
+            int header = reader.ReadInt32();
+            if (header == 0)
+            {
+                hexGrid.Load(reader);
+            }
+            else
+            {
+                Debug.LogWarning("Unknown map format " + header);
+            }
         }
     }
 }
