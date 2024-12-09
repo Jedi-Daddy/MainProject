5e975f8eb0a512d3657f6683bebf47b723900321
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-12-09 17:48:43 +0100

    instant path

--- Zeus/Assets/Scripts/UI/HexMapEditor.cs
+++ Zeus/Assets/Scripts/UI/HexMapEditor.cs
@@ -180,21 +180,27 @@ public class HexMapEditor : MonoBehaviour
             }
             else if (Input.GetKey(KeyCode.LeftShift))
             {
-                if (searchFromCell)
+                if (searchFromCell != currentCell)
                 {
-                    searchFromCell.DisableHighlight();
-                }
-                searchFromCell = currentCell;
-                searchFromCell.EnableHighlight(Color.blue);
-                if (searchToCell)
-                {
-                    hexGrid.FindPath(searchFromCell, searchToCell, 24);
+                    if (searchFromCell)
+                    {
+                        searchFromCell.DisableHighlight();
+                    }
+                    searchFromCell = currentCell;
+                    searchFromCell.EnableHighlight(Color.blue);
+                    if (searchToCell)
+                    {
+                        hexGrid.FindPath(searchFromCell, searchToCell, 24);
+                    }
                 }
             }
             else if (searchFromCell && searchFromCell != currentCell)
             {
-                searchToCell = currentCell;
-                hexGrid.FindPath(searchFromCell, searchToCell, 24);
+                if (searchToCell != currentCell)
+                {
+                    searchToCell = currentCell;
+                    hexGrid.FindPath(searchFromCell, searchToCell, 24);
+                }
             }
             previousCell = currentCell;
         }
