13a4870ab3a95ef25eecec68f22a3cf215752b3e
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-12-09 18:27:08 +0100

    add orientation for Unit and moving

--- Zeus/Assets/Scripts/HexUnit.cs
+++ Zeus/Assets/Scripts/HexUnit.cs
@@ -6,6 +6,7 @@ using System.IO;
 public class HexUnit : MonoBehaviour
 {
 
+    const float rotationSpeed = 180f;
     const float travelSpeed = 4f;
 
     public static HexUnit unitPrefab;
@@ -68,6 +69,8 @@ public class HexUnit : MonoBehaviour
     IEnumerator TravelPath()
     {
         Vector3 a, b, c = pathToTravel[0].Position;
+        transform.localPosition = c;
+        yield return LookAt(pathToTravel[1].Position);
 
         float t = Time.deltaTime * travelSpeed;
         for (int i = 1; i < pathToTravel.Count; i++)
@@ -78,6 +81,9 @@ public class HexUnit : MonoBehaviour
             for (; t < 1f; t += Time.deltaTime * travelSpeed)
             {
                 transform.localPosition = Bezier.GetPoint(a, b, c, t);
+                Vector3 d = Bezier.GetDerivative(a, b, c, t);
+                d.y = 0f;
+                transform.localRotation = Quaternion.LookRotation(d);
                 yield return null;
             }
             t -= 1f;
@@ -89,8 +95,39 @@ public class HexUnit : MonoBehaviour
         for (; t < 1f; t += Time.deltaTime * travelSpeed)
         {
             transform.localPosition = Bezier.GetPoint(a, b, c, t);
+            Vector3 d = Bezier.GetDerivative(a, b, c, t);
+            d.y = 0f;
+            transform.localRotation = Quaternion.LookRotation(d);
             yield return null;
         }
+
+        transform.localPosition = location.Position;
+        orientation = transform.localRotation.eulerAngles.y;
+        ListPool<HexCell>.Add(pathToTravel);
+        pathToTravel = null;
+    }
+
+    IEnumerator LookAt(Vector3 point)
+    {
+        point.y = transform.localPosition.y;
+        Quaternion fromRotation = transform.localRotation;
+        Quaternion toRotation =
+            Quaternion.LookRotation(point - transform.localPosition);
+        float speed = rotationSpeed / Quaternion.Angle(fromRotation, toRotation);
+
+        for (
+            float t = Time.deltaTime * speed;
+            t < 1f;
+            t += Time.deltaTime * speed
+        )
+        {
+            transform.localRotation =
+                Quaternion.Slerp(fromRotation, toRotation, t);
+            yield return null;
+        }
+
+        transform.LookAt(point);
+        orientation = transform.localRotation.eulerAngles.y;
     }
 
     public void Die()
@@ -122,32 +159,27 @@ public class HexUnit : MonoBehaviour
         }
     }
 
-    void OnDrawGizmos()
-    {
-        if (pathToTravel == null || pathToTravel.Count == 0)
-        {
-            return;
-        }
-
-        Vector3 a, b, c = pathToTravel[0].Position;
-
-        for (int i = 1; i < pathToTravel.Count; i++)
-        {
-            a = c;
-            b = pathToTravel[i - 1].Position;
-            c = (b + pathToTravel[i].Position) * 0.5f;
-            for (float t = 0f; t < 1f; t += 0.1f)
-            {
-                Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
-            }
-        }
-
-        a = c;
-        b = pathToTravel[pathToTravel.Count - 1].Position;
-        c = b;
-        for (float t = 0f; t < 1f; t += 0.1f)
-        {
-            Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
-        }
-    }
+    //	void OnDrawGizmos () {
+    //		if (pathToTravel == null || pathToTravel.Count == 0) {
+    //			return;
+    //		}
+    //
+    //		Vector3 a, b, c = pathToTravel[0].Position;
+    //
+    //		for (int i = 1; i < pathToTravel.Count; i++) {
+    //			a = c;
+    //			b = pathToTravel[i - 1].Position;
+    //			c = (b + pathToTravel[i].Position) * 0.5f;
+    //			for (float t = 0f; t < 1f; t += 0.1f) {
+    //				Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
+    //			}
+    //		}
+    //
+    //		a = c;
+    //		b = pathToTravel[pathToTravel.Count - 1].Position;
+    //		c = b;
+    //		for (float t = 0f; t < 1f; t += 0.1f) {
+    //			Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
+    //		}
+    //	}
 }
